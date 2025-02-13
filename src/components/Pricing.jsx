6e8f894efae2bf6d10fbd01e904e0c5f12611320
Jahnvi Verma <vermajaanvi64@gmail.com>
2025-02-13 22:10:46 +0530

    DONE

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,7 +5,7 @@ const Pricing = () => {
   return (
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wide">
-        Pricing
+        Register for the event
       </h2>
       <div className="flex flex-wrap">
         {pricingOptions.map((option, index) => (
@@ -21,7 +21,6 @@ const Pricing = () => {
               </p>
               <p className="mb-8">
                 <span className="text-5xl mt-6 mr-2">{option.price}</span>
-                <span className="text-neutral-400 tracking-tight">/Month</span>
               </p>
               <ul>
                 {option.features.map((feature, index) => (
@@ -35,7 +34,7 @@ const Pricing = () => {
                 href="#"
                 className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200"
               >
-                Subscribe
+                Register
               </a>
             </div>
           </div>
